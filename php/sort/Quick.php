de6ee47e8c8724876835d08eb44dc1b8fb1662bf
dengzhiyong <dengzhiyong@bilibili.com>
2016-06-29 10:28:50 +0800

    add quick sort algorithm

---
+++ php/sort/Quick.php
@@ -0,0 +1,43 @@
+<?php
+require ("AbstractSort.php");
+class Quick extends AbstractSort
+{
+
+	public static function sort(array $input)
+	{
+		self::_quick($input, 0, count($input) - 1);
+		return $input;
+	}
+
+	private static function _quick(&$input, $left, $right)
+	{
+		if ($left > $right) 
+			return;
+	    
+	    $base = $input[$left];
+	    $i = $left;
+	    $j = $right;
+
+	    while ($i != $j) {
+	    	while ($input[$j] >= $base && $i < $j)
+	    		$j--;
+
+	    	while ($input[$i] <= $base && $i < $j)
+	    		$i++;
+
+	    	if ($i < $j) {
+	    		$tmp = $input[$i];
+	    		$input[$i] = $input[$j];
+	    		$input[$j] = $tmp;
+	    	}
+	    }
+
+	    $input[$left] = $input[$i];
+	    $input[$i] = $base;
+
+	    self::_quick($input, $left, $i-1);
+	    self::_quick($input, $i+1, $right);
+	}
+}
+
+Quick::main();
